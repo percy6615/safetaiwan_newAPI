bf51e9ed0e84ebc219e8eaf79b3cbe860ff1c139
percy6615 <percy6615_@hotmail.com>
2017-04-05 11:28:44 +0800

    dbfunction

---
+++ src/safetaiwan_CommTools/DBSource/DBSource.java
@@ -0,0 +1,9 @@
+package safetaiwan_CommTools.DBSource;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public interface DBSource {
+    public Connection getConnection() throws SQLException;
+    public void closeConnection(Connection conn) throws SQLException;
+}
